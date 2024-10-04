da3e8c68267925b8085398ccd6fe98e8085b2a1f
WIktoria <wiki@gmail.com>
2024-10-04 09:59:54 +0200

    Init

---
+++ proj1/WebApplication1/WebApplication1/Pages/Kotki.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class KotkiModel : PageModel
+    {
+        private readonly ILogger<KotkiModel> _logger;
+
+        public KotkiModel(ILogger<KotkiModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
